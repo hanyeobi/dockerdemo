7fca84a1673e952c6a555f7c2dc70b3c45db1010
hanyeobi <hanyeobi@gmail.com>
2023-02-07 17:40:38 +0900

    init commit

---
+++ src/main/java/com/hyp/handemo1/Handemo1Application.java
@@ -0,0 +1,13 @@
+package com.hyp.handemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Handemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Handemo1Application.class, args);
+	}
+
+}
